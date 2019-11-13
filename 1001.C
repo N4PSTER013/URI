0d550777f2a8788414f850d5fdcea1c80f16a293
N4PSTER013 <56977028+N4PSTER013@users.noreply.github.com>
2019-11-13 01:10:15 -0300

    Create 1001.C

---
+++ 1001.C
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+	int A;
+	int B;
+	int X;
+
+	scanf("%d \n", &A);
+	scanf("%d \n", &B);
+	X = A + B;
+	printf("X = %d\n", X);
+ 
+    return 0;
+}
