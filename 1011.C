a6ca8a9107d304ee609f176194b66c7d39bdc129
N4PSTER013 <56977028+N4PSTER013@users.noreply.github.com>
2019-11-15 00:39:34 -0300

    Create 1011.C

---
+++ 1011.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * 3.14159
+     */
+     
+     float r;
+     scanf(" %f", &r);
+     
+     printf("VOLUME = %.3f\n", (4.0/3) * 3.14159 * r * r * r);
+ 
+    return 0;
+}
